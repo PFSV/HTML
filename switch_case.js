4bd821ca22d87288e70b2d6be26efbe0520df0af
PFSV <96779838+PFSV@users.noreply.github.com>
2022-05-29 12:30:05 +0900

    Add files via upload
    javasript onclick

---
+++ switch_case.js
@@ -0,0 +1,14 @@
+let order = "";
+
+switch (order) {
+  case "frappucino":
+    console.log("프라푸치노 나왔습니다.");
+    break;
+  case "latte":
+    console.log("우유 나왔습니다.");
+    break;
+  case "coffee":
+    console.log("커피 나왔습니다.");
+  default:
+    console.log("메뉴판에 있는 걸로 주문을 해 주십시오")
+}
